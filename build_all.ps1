11f64097296d5ecded6bb8d20272050cc5139f17
Ashish Negi <asnegi@microsoft.com>
2018-08-27 20:45:04 +0530

    Add generation of nupkg

--- build_all.ps1
+++ build_all.ps1
@@ -1,2 +1,15 @@
-# build all projects and generate nupkg
+# build all projects
 dotnet build service-fabric-backup-explorer.sln
+
+# publish for nupkg generation
+pushd RCBackupParser
+dotnet publish
+popd
+
+# nuget restore
+nuget restore service-fabric-backup-explorer.sln
+
+# generate nupkg
+pushd nuprojs
+msbuild Microsoft.ServiceFabric.Tools.RCBackupParser.nuproj
+popd
