2da58212c8b825d282022da67e7fe17cbe3728e9
Ashish Negi <asnegi@microsoft.com>
2018-09-05 13:48:28 +0530

    Add headers

--- src/Microsoft.ServiceFabric.ReliableCollectionBackup/Parser.Tests/GenericsTests.cs
+++ src/Microsoft.ServiceFabric.ReliableCollectionBackup/Parser.Tests/GenericsTests.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.ServiceFabric.Data.Collections;
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Data.Collections.ReliableConcurrentQueue;
 using Microsoft.ServiceFabric.ReliableCollectionBackup.Parser;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
--- src/Microsoft.ServiceFabric.ReliableCollectionBackup/Parser/NotifyTransactionAppliedEventArgs.cs
+++ src/Microsoft.ServiceFabric.ReliableCollectionBackup/Parser/NotifyTransactionAppliedEventArgs.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.ServiceFabric.Data;
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using Microsoft.ServiceFabric.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
