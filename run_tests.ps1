b7bfbe40aa80e95415f43b37526532025232a2ca
Ashish Negi <asnegi@microsoft.com>
2018-09-10 13:49:49 +0530

    Fixed scripts

--- run_tests.ps1
+++ run_tests.ps1
@@ -7,3 +7,9 @@ dotnet --info
 # --logger is supposed to print console logs but it does not work.
 dotnet test --no-build --diag test_results.log --verbosity normal --logger "console;verbosity=detailed"
 popd
+
+# run rest server tests
+pushd src\Microsoft.ServiceFabric.ReliableCollectionBackup\RestServer.Tests
+xcopy.exe /EIYS ..\..\..\packages\microsoft.servicefabric.tools.reliabilitysimulator\6.4.187-beta\lib\netstandard2.0\*.dll bin\Debug\net471\
+dotnet test --no-build --diag test_results.log --verbosity normal --logger "console;verbosity=detailed"
+popd
