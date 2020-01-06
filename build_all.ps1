100f5505fd01df239b9946449912b8f74e26bac2
Saket Harsh <saharsh@microsoft.com>
2020-01-06 11:13:21 +0530

    Corrected Build Script

--- build_all.ps1
+++ build_all.ps1
@@ -1,22 +1,27 @@
 ##
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+##
+
+##
 #  Builds the source code and generates nuget packages. You can optionally just build the source code by opening individual solutions in Visual Studio.
 ##
 
 param
 (
-    # show versions of all tools used
+    # Show versions of all tools used
     [switch]$showVersion,
 
-    # build code
+    # Build Code
     [switch]$build,
 
-    # generate nupkg
+    # Generate NuPkg
     [switch]$generateNupkg,
 
-    # generate nupkg
+    # Do All the Above tasks together
     [switch]$buildAll,
     
-    # generate nupkg
+    # Build the bkpctl Cli
     [switch]$buildCli
 );
 
@@ -36,7 +41,7 @@ if ($showVersion) {
 if ($build -Or $buildAll) {
     $DisplayHelp = $false;
     # build all projects
-    Write-Host "Building code and tests:"
+    Write-Host "Building Backup Explorer Code and Tests:"
     Exec { dotnet build --packages .\packages service-fabric-backup-explorer.sln }
 
     # publish for nupkg generation
@@ -61,9 +66,9 @@ if ($generateNupkg -Or $buildAll) {
     # nuget restore
     Exec { nuget.exe restore -Verbosity detailed .nuget\packages.config -PackagesDirectory .\packages }
     # generate nupkg
-    Exec { msbuild Microsoft.ServiceFabric.ReliableCollectionBackup.Parser.nuproj /p:OutputPath=..\bin\nupkg }
+    Exec { MSBuild.exe Microsoft.ServiceFabric.ReliableCollectionBackup.Parser.nuproj /p:OutputPath=..\bin\nupkg }
     popd
-}
+} 
 
 if ($buildCli -Or $buildAll) {
     $DisplayHelp = $false;
@@ -81,4 +86,5 @@ if ($DisplayHelp) {
     Write-Host "2. -buildAll Builds the Code and generate Nuget package"
     Write-Host "3. -generateNupkg Generate the nuget package"
     Write-Host "4. -showVersion Displays versions of all tools"
+    Write-Host "5. -buildCli Builds the bkpctl CLI tool for viewing and editing Backups"
 }
