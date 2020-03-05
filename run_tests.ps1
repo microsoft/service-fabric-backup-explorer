101d636edf3925886d0641eb9b3272db86c78747
Saket Harsh <saharsh@microsoft.com>
2020-03-05 10:57:27 +0530

    Pipeline fix for release bits (#19)

--- run_tests.ps1
+++ run_tests.ps1
@@ -8,7 +8,7 @@ Exec { dotnet --info }
 # --no-build make sure we don't waste time in building again.
 # --verbosity help us know which test passed.
 # --logger is supposed to print console logs but it does not work.
-Exec { dotnet test --no-build --diag test_results.log --verbosity normal --logger "console;verbosity=detailed" }
+Exec { dotnet test --no-build --diag test_results.log --verbosity normal --logger "console;verbosity=detailed" --configuration Release }
 popd
 
 # Uncomment these lines to run Rest Server Tests locally. 
